f8ab6bf925e6edb09be7702bb3ba1cd6f5677383
zhengpingli99 <zhengpingli99@126.com>
2019-09-27 17:11:56 +0800

    集成 kafka

---
+++ demo/src/main/java/com/example/demo/common/KafkaProducer.java
@@ -0,0 +1,33 @@
+package com.example.demo.common;
+
+import com.example.demo.entity.Message;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Gson gson = new GsonBuilder().create();
+
+    //发送消息方法
+   public void send() {
+     Message message = new Message();
+     message.setId(System.currentTimeMillis());
+     message.setMsg(UUID.randomUUID().toString());
+     message.setSendTime(new Date());
+     log.info("+++++++++++++++++++++  message = {}", gson.toJson(message));
+   //topic-ideal为主题
+     kafkaTemplate.send("topic-ideal", gson.toJson(message));
+    }
+
+}
